6eade30958933896b49bfb1a453a5dc41a97f4da
Formic Acid <formicacidgd@gmail.com>
2026-06-14 16:30:17 +0200

    bro i am never using github desktop again

--- src/main/java/at/hannibal2/skyhanni/config/features/misc/MiscConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/misc/MiscConfig.kt
@@ -485,7 +485,7 @@ class MiscConfig {
     @ConfigOption(name = "Color Particle Warning", desc = "§c§lThis can break particle Coloring in parts of Skyblock where it is done properly.§f")
     @ConfigEditorInfoText
     @SearchTag("Fixes Hypixel not setting colored particles properly such as Slayer Specific Spawn Particles or Motes Fix Colored Particles")
-    val notice: String = ""
+    var notice: String = ""
 
     @Expose
     @ConfigOption(
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -121,7 +121,7 @@ allprojects {
     }
 }
 
-stonecutter active "26.1"
+stonecutter active "1.21.11"
 
 stonecutter handlers {
     configure("fsh", "vsh") {
