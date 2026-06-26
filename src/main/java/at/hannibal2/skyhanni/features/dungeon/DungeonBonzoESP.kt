96005f530df287e9db5fd3e0fe5aea261bfdc7f9
Formic Acid <formicacidgd@gmail.com>
2026-06-26 23:10:38 +0200

    idk

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonBonzoESP.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonBonzoESP.kt
@@ -21,44 +21,44 @@ import java.awt.Color
 
 @SkyHanniModule
 object DungeonBonzoESP {
-    private val config get() = SkyHanniMod.feature.dungeon.bonzoESP
-    var bonzo: RemotePlayer? = null
-        private set
-
-    const val BONZO: String = "ewogICJ0aW1lc3RhbXAiIDogMTYyMTEzNDcwMTk3MCwKICAicHJvZmlsZUlkIiA6ICI5ZDIyZGRhOTVmZGI0MjFmOGZhNjAzNTI1YThkZmE4ZCIsCiAgInByb2ZpbGVOYW1lIiA6ICJTYWZlRHJpZnQ0OCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS85MWU3OWY3ZTYyOGYyMWY0Mjc2MTBjYzZjZWQwZmYxNTNlMjE4ODBkNjFjYmFjNjczYmY4NjJiMWQ0ZTQ0MjZiIiwKICAgICAgIm1ldGFkYXRhIiA6IHsKICAgICAgICAibW9kZWwiIDogInNsaW0iCiAgICAgIH0KICAgIH0KICB9Cn0="
-
-    const val DEAD_BONZO: String = "ewogICJ0aW1lc3RhbXAiIDogMTYyMjg1NTk2MzUyNCwKICAicHJvZmlsZUlkIiA6ICIyMDA2NTVkMjMyYTE0MTc2OGIwNjQ0NWNkNTliNDg3NCIsCiAgInByb2ZpbGVOYW1lIiA6ICJGaWVzdHlCbHVlXyIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS8xMjcxNmVjYmY1YjhkYTAwYjA1ZjMxNmVjNmFmNjFlOGJkMDI4MDViMjFlYjhlNDQwMTUxNDY4ZGM2NTY1NDljIgogICAgfQogIH0KfQ=="
-
-    val c = Color(255, 0, 0);
-
-    @OptIn(AllEntitiesGetter::class)
-    @HandleEvent(SecondPassedEvent::class)
-    fun onSecondPassed() {
-        if (!config.enabled.get()) return
-        if (!(DungeonApi.inBossRoom && DungeonApi.getCurrentBoss() == DungeonFloor.F1)) return
-
-        val entity = EntityUtils.getEntities<RemotePlayer>()
-            .firstOrNull { it.getSkinTexture() == BONZO || it.getSkinTexture() == DEAD_BONZO }
-            ?: return
-        RenderLivingEntityHelper.setEntityColor(
-            entity,
-            color = config.color.toColor(),
-            condition = { true },
-        )
-    }
-
-    @HandleEvent(SkyHanniRenderWorldEvent::class)
-    fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
-        if (!config.enabled.get()) return
-
-        if (!(DungeonApi.inBossRoom && DungeonApi.getCurrentBoss() == DungeonFloor.F1)) return
-        val entity = bonzo ?: return
-
-        val location = event.exactLocation(entity)
-        val boundingBox = event.exactBoundingBox(entity)
-
-        event.drawFilledBoundingBox(boundingBox, c, 0.5f)
-        event.drawLineToCrosshair(location.add(0.5, 0.5), c, 3, true)
-
-    }
+//     private val config get() = SkyHanniMod.feature.dungeon.bonzoESP
+//     var bonzo: RemotePlayer? = null
+//         private set
+//
+//     const val BONZO: String = "ewogICJ0aW1lc3RhbXAiIDogMTYyMTEzNDcwMTk3MCwKICAicHJvZmlsZUlkIiA6ICI5ZDIyZGRhOTVmZGI0MjFmOGZhNjAzNTI1YThkZmE4ZCIsCiAgInByb2ZpbGVOYW1lIiA6ICJTYWZlRHJpZnQ0OCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS85MWU3OWY3ZTYyOGYyMWY0Mjc2MTBjYzZjZWQwZmYxNTNlMjE4ODBkNjFjYmFjNjczYmY4NjJiMWQ0ZTQ0MjZiIiwKICAgICAgIm1ldGFkYXRhIiA6IHsKICAgICAgICAibW9kZWwiIDogInNsaW0iCiAgICAgIH0KICAgIH0KICB9Cn0="
+//
+//     const val DEAD_BONZO: String = "ewogICJ0aW1lc3RhbXAiIDogMTYyMjg1NTk2MzUyNCwKICAicHJvZmlsZUlkIiA6ICIyMDA2NTVkMjMyYTE0MTc2OGIwNjQ0NWNkNTliNDg3NCIsCiAgInByb2ZpbGVOYW1lIiA6ICJGaWVzdHlCbHVlXyIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS8xMjcxNmVjYmY1YjhkYTAwYjA1ZjMxNmVjNmFmNjFlOGJkMDI4MDViMjFlYjhlNDQwMTUxNDY4ZGM2NTY1NDljIgogICAgfQogIH0KfQ=="
+//
+//     val c = Color(255, 0, 0);
+//
+//     @OptIn(AllEntitiesGetter::class)
+//     @HandleEvent(SecondPassedEvent::class)
+//     fun onSecondPassed() {
+//         if (!config.enabled.get()) return
+//         if (!(DungeonApi.inBossRoom && DungeonApi.getCurrentBoss() == DungeonFloor.F1)) return
+//
+//         val entity = EntityUtils.getEntities<RemotePlayer>()
+//             .firstOrNull { it.getSkinTexture() == BONZO || it.getSkinTexture() == DEAD_BONZO }
+//             ?: return
+//         RenderLivingEntityHelper.setEntityColor(
+//             entity,
+//             color = config.color.toColor(),
+//             condition = { true },
+//         )
+//     }
+//
+//     @HandleEvent(SkyHanniRenderWorldEvent::class)
+//     fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
+//         if (!config.enabled.get()) return
+//
+//         if (!(DungeonApi.inBossRoom && DungeonApi.getCurrentBoss() == DungeonFloor.F1)) return
+//         val entity = bonzo ?: return
+//
+//         val location = event.exactLocation(entity)
+//         val boundingBox = event.exactBoundingBox(entity)
+//
+//         event.drawFilledBoundingBox(boundingBox, c, 0.5f)
+//         event.drawLineToCrosshair(location.add(0.5, 0.5), c, 3, true)
+//
+//     }
 }
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -122,7 +122,7 @@ allprojects {
     }
 }
 
-stonecutter active "1.21.11"
+stonecutter active "26.1"
 
 stonecutter handlers {
     configure("fsh", "vsh") {
